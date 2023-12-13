4fe638a773b25b5074bf1bbef7965d9169e64ddb
StasonJatham <machleidtk@gmail.com>
2023-12-13 20:34:30 +0100

    made info pretty

--- src/components/management/Storage.jsx
+++ src/components/management/Storage.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Chart from "react-apexcharts";
-import {
-    Typography
-} from "@material-tailwind/react";
 
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    Typography,
+    Tooltip
+} from "@material-tailwind/react";
 import endpoints from "../../data/endpoints.json";
 
 export default function Storage() {
@@ -26,6 +30,10 @@ export default function Storage() {
         try {
             setLoadingState(true);
             const response = await axios.get(endpoints.settings.storage);
+            response.data.storage_total = parseFloat(response.data.storage_total.toFixed(2))
+            response.data.storage_free = parseFloat(response.data.storage_free.toFixed(2))
+            response.data.ram_used = parseFloat(response.data.ram_used.toFixed(2))
+            response.data.ram_total = parseFloat(response.data.ram_total.toFixed(2))
             setStorage(response.data);
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -46,7 +54,7 @@ export default function Storage() {
                     }
                 },
             },
-            colors: ['#39539E'],
+            colors: ['#000000'],
             labels: ['RAM'],
         },
     };
@@ -64,29 +72,29 @@ export default function Storage() {
                     }
                 },
             },
-            colors: ['#39539E'],
+            colors: ['#000000'],
             labels: ['Storage'],
         },
     };
     return (
         <>
             <div className='flex'>
-
-
-                <div>
-                    <Chart {...chartConfig} />
-                    <Typography className='text-center'>
-                        {storage.storage_total} GB / {storage.storage_free} GB
-                    </Typography>
-                </div>
-
-                <div className=''>
-                    <Chart {...ramChart} />
-                    <Typography className='text-center'>
-                        {storage.ram_used} GB / {storage.ram_total} GB
-                    </Typography>
-                </div>
-
+                <Card className="md:w-6/12 lg:w-4/12 m-2">
+                    <CardBody className="pt-2 px-2 pb-0">
+                        <Chart {...chartConfig} />
+                        <Typography className='text-center'>
+                            {storage.storage_total} GB / {storage.storage_free} GB
+                        </Typography>
+                    </CardBody>
+                </Card>
+                <Card className="md:w-6/12 lg:w-4/12 m-2">
+                    <CardBody className="pt-2 px-2 pb-0">
+                        <Chart {...ramChart} />
+                        <Typography className='text-center'>
+                            {storage.ram_used} GB / {storage.ram_total} GB
+                        </Typography>
+                    </CardBody>
+                </Card>
             </div >
         </>
     );
